227d08f138d172bf5df155cf7cd5698b238d69a1
Guguinha <gustavo.ferreira0821@gmail.com>
2025-07-27 11:22:39 -0300

    :lipstick: home page style added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,32 @@
-export function Home(){
-    return (
-        <div>
-            Home
-        </div>
-    )
+// src/pages/Home/index.tsx
+import { Link } from "react-router-dom"; // Importe Link
+import { mockRubrics } from "../../data/mockRubrics"; // Importe os dados fictícios
+import styles from "./Home.module.scss"; // Importe os estilos
+
+export function Home() {
+  return (
+    <div className={styles.homeContainer}>
+      <h1 className={styles.title}>My Rubrics</h1>
+      <div className={styles.rubricGrid}>
+        {/* Card para Adicionar Nova Rúbrica */}
+        <Link to="/rubric" className={styles.addRubricCard}>
+          <span className={styles.plusIcon}>+</span>
+        </Link>
+
+        {/* Cards de Rúbricas Existentes */}
+        {mockRubrics.map((rubric) => (
+          <Link
+            key={rubric.id}
+            to={`/rubric?id=${rubric.id}`} // Exemplo: passar ID via query param
+            className={styles.rubricCard}
+          >
+            <h2 className={styles.rubricName}>{rubric.title}</h2>
+            <p className={styles.studentCount}>
+              {rubric.numberOfAssignedStudents} Students
+            </p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
